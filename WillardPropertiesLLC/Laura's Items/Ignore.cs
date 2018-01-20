0d14303cb88699569f9266624018faa4dc33691b
Nsteinbaugh <ste19@live.missouristate.edu>
2018-01-19 22:49:14 -0600

    Add Laura's files to repo

---
+++ WillardPropertiesLLC/Laura's Items/Ignore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WillardPropertiesLLC.Laura_s_Items
+{
+    public class Ignore
+    {
+    }
+}
---
+++ WillardPropertiesLLC/Laura's Items/properties.js
@@ -0,0 +1,213 @@
+//List of properties
+var properties = [
+	{
+		"id": 0,
+		"title": "7030 West Dogwood Street Springfield, Missouri 65803",
+		"latitude":37.2484528,
+		"longitude":-93.42896719999999,
+		"image":"7030WestDogwoodStreet.jpg",
+		"description":"7030 West Dogwood Street",
+		"link":"7030WestDogwood.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 1,
+		"title": "West Farm Road 106 Unit A Springfield, Missouri 65803",
+		"latitude":37.2560641,
+		"longitude":-93.35758379999999,
+		"image":"WestFarmRoad106UnitA.jpg",
+		"description":"West Farm Road 106 Unit A",
+		"link":"WestFarmRoad106UnitA.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 2,
+		"title": "West Farm Road 106 Unit B Springfield, Missouri 65803",
+		"latitude":37.2560641,
+		"longitude":-93.35758379999999,
+		"image":"WestFarmRoad106UnitB.jpg",
+		"description":"West Farm Road 106 Unit B",
+		"link":"WestFarmRoad106UnitB.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 3,
+		"title": "200 New Melville Road Willard MO, 65781",
+		"latitude":37.295535,
+		"longitude":-93.42848500000002,
+		"image":"200NewMelvilleRoad.jpg",
+		"description":"200 New Melville Road",
+		"link":"200NewMelvilleRoad.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 4,
+		"title": "3274 North State Highway AB Unit A Springfield, Missouri 65803",
+		"latitude":37.2587263,
+		"longitude":-93.42989030000001,
+		"image":"3274NorthStateHighwayABunita.jpg",
+		"description":"3274 North State Highway AB Unit A",
+		"link":"3274NStateHwyABUnitA.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 5,
+		"title": "3274 North State Highway AB Unit B Springfield, Missouri 65803",
+		"latitude":37.2587263,
+		"longitude":-93.42989030000001,
+		"image":"3274NStateHighwayABunitb.jpg",
+		"description":"3274 North State Highway AB Unit B",
+		"link":"3274NStateHwyABUnitB.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 6,
+		"title": "203 Kings Drive Willard, Missouri 65781",
+		"latitude":37.29488089999999,
+		"longitude":-93.42755599999998,
+		"image":"203KingsDriveWillard.jpg",
+		"description":"203 Kings Drive",
+		"link":"203KingsDrive.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 7,
+		"title": "402 Howard Street Willard, Missouri 65781",
+		"latitude":37.3008529,
+		"longitude": -93.42772330000003,
+		"image":"402HowardStreetWillard.jpg",
+		"description":"402 Howard Street",
+		"link":"402HowardStreet.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 8,
+		"title": "202 South Street Willard, Missouri 65781",
+		"latitude":37.3028521,
+		"longitude": -93.42837199999997,
+		"image":"202SouthStreet.jpg",
+		"description":"202 South Street",
+		"link":"202SouthStreet.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 9,
+		"title": "200 E Robberson Street Willard, Missouri 65781",
+		"latitude": 37.3032256,
+		"longitude": -93.42796570000002,
+		"image":"200ERobbersonStreet.jpg",
+		"description":"200 E Robberson Street",
+		"link":"200ERobbersonStreet.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 10,
+		"title": "310 Pershing Street Willard, Missouri 65781",
+		"latitude":37.3023297,
+		"longitude": -93.42941209999998,
+		"image":"310PershingStreet.jpg",
+		"description":"310 Pershing Street",
+		"link":"310PershingStreet.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 11,
+		"title": "108 Mill Street Willard, Missouri 65781 ",
+		"latitude":37.305511,
+		"longitude":-93.42649499999999,
+		"image":"108MillStreet.jpg",
+		"description":"108 Mill Street",
+		"link":"108MillStreet.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 12,
+		"title": "Property 13",
+		"latitude":37.792401,
+		"longitude": -122.404370,
+		"image":"http://placehold.it/760x670",
+		"description":"Kearny St, San Francisco, CA <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 13,
+		"title": "Property 14",
+		"latitude":37.792905,
+		"longitude": -122.410229,
+		"image":"http://placehold.it/760x670",
+		"description":"Sacramento St, San Francisco, CA <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 14,
+		"title": "Property 15",
+		"latitude":37.792486,
+		"longitude": -122.406139,
+		"image":"http://placehold.it/760x670",
+		"description":"Sacramento St, San Francisco, CA <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 15,
+		"title": "Property 16",
+		"latitude":37.795108,
+		"longitude": -122.400364,
+		"image":"http://placehold.it/760x670",
+		"description":"California St, San Francisco, CA <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 16,
+		"title": "Property 17",
+		"latitude":36.139670,
+		"longitude": -86.786006,
+		"image":"http://placehold.it/760x670",
+		"description":"11th Ave S, Nashville, TN <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 17,
+		"title": "Property 18",
+		"latitude":36.140701,
+		"longitude": -86.793153,
+		"image":"http://placehold.it/760x670",
+		"description":"Horton Ave, Nashville, TN <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 18,
+		"title": "Property 19",
+		"latitude":29.424113,
+		"longitude": -98.494830,
+		"image":"http://placehold.it/760x670",
+		"description":"Dolorosa, San Antonio, TX <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 19,
+		"title": "Property 20",
+		"latitude":33.440884,
+		"longitude": -112.073876,
+		"image":"http://placehold.it/760x670",
+		"description":"Central Ave, Phoenix, AZ <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	},
+	{
+		"id": 20,
+		"title": "Property 21",
+		"latitude":39.099850,
+		"longitude": -94.578720,
+		"image":"http://placehold.it/760x670",
+		"description":"12th St, Kansas City, MO <br> Phone: (123) 546-7890",
+		"link":"property-details.html",
+		"map_marker_icon":"house_markers.png"
+	}
+];
