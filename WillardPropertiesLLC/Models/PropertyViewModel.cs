9b3120737052ab804aefc99560fbc17fab99b229
Nsteinbaugh <ste19@live.missouristate.edu>
2018-01-25 12:15:05 -0600

    Add json file and viewmodel for properties

---
+++ WillardPropertiesLLC/Models/PropertyViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WillardPropertiesLLC.Models
+{
+    public class PropertyViewModel
+    {
+        private int description { get; set; }
+        private string address { get; set; }
+        private string image { get; set; }
+        private string numberOfBeds { get; set; }
+        private string numberOfBaths { get; set; }
+        private bool forRent { get; set; }
+        private double rentAmount { get; set; }
+    }
+}
